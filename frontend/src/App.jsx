7045c780ce9924a9eb89b8dfbaf0db19b0702ba4
w w k soyza <it22060594@my.sliit.lk>
2025-04-18 14:11:13 +0530

    challenge crud commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register';
 import LoginPage from './components/LoginPage';
 import ProfilePage from './components/ProfilePage';
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/ProtectedRoute';   
+import AddChallenge from './components/Challenges/AddChallenge';
+import QuizManagement from './components/Challenges/QuizManagement';
+import EditChallenge from './components/Challenges/EditChallenge';
+import Home from './components/Home/Home';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+
+      <Route path="/" element={<Home />} /> {/*  Home as default route */}
+     
+
+       {/* User Registation and Login */}
+
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
@@ -19,7 +29,17 @@ const App = () => {
             </ProtectedRoute>
           }
         />
+        
+
+         {/* Challenge */}
+         <Route path="/add-challenge" element={<AddChallenge />} />
+         <Route path="/all-challenge" element={<QuizManagement />} />
+         <Route path="/update-challenge/:challengeId" element={<EditChallenge />} />
+
+
       </Routes>
+
+      
     </Router>
   );
 };
